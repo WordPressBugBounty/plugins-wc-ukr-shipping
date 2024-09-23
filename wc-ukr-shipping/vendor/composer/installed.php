78f9af6fc48faf568a65636c079d64fad330e058
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 05:14:37 +0000

    1.12.5

---
+++ wc-ukr-shipping/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '413344e2b6f943bb16ca7cf9ff7c8127b6995efd',
+        'name' => 'kirillbdev/wc-ukr-shipping',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'kirillbdev/wc-ukr-shipping' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '413344e2b6f943bb16ca7cf9ff7c8127b6995efd',
+            'dev_requirement' => false,
+        ),
+        'kirillbdev/wcus-core' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../kirillbdev/wcus-core',
+            'aliases' => array(),
+            'reference' => 'd5b5021a7f256d933b761fe137c2ae10a3b9a5b7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
